96f29723450b6c551a7c5470ad836f3245c92c7c
Dario Dorando <dariodorando@gmail.com>
2016-02-04 15:31:52 +0100

    Create pException.cpp

---
+++ stdlib/pException.cpp
@@ -0,0 +1,18 @@
+#include "pException.h"
+#include <algorithm>
+#include <iostream>
+#include <cstdbool>
+
+int exceptHandler(int exceptcode) {
+	int handlecode;
+	bool isfatal;
+	perr.errno->exceptcode;
+	if (exceptcode = 0)
+	{
+		cerr << "";
+		isfatal = false;
+		handlecode = 0;
+	}
+	if (exceptcode = 1)
+	{
+		cerr << ""
