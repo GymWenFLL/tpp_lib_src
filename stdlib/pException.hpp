f6f697d2df22fd97afd1c0b5bc8c28185d8eca3b
Dario Dorando <dariodorando@gmail.com>
2016-02-04 15:31:23 +0100

    Create pException.hpp

---
+++ stdlib/pException.hpp
@@ -0,0 +1,27 @@
+#ifndef pException_hpp_00_02_
+#define pException_hpp_00_02_
+
+*/
+/ This file defines the Exception-Codes!
+/ Normal behaviour   = 0 (norm)
+/ File I/O Error     = 1 (fioe)
+/ Processor Error    = 2 (prce)
+/ Memory Error       = 3 (meme)
+/ Code Error         = 4 (code)
+/ Network Error      = 5 (nete)
+/ Program Error      = 6 (prge)
+/ Process Error      = 7 (prse)
+/ Security Error     = 8 (sece)
+/ Security Violation = 9 (secv)
+/*
+
+#define norm 0
+#define fioe 1
+#define prce 2
+#define meme 3
+#define code 4
+#define nete 5
+#define prge 6
+#define prse 7
+#define sece 8
+#define secv 9
